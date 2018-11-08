ab4506d9fc3ff710989f08329b9ddf5e3c16c633
Ezra Kiteck <ezrakiteck@Interapts-MacBook-Pro-46.local>
2018-11-08 10:26:20 -0600

    Initial Commit

---
+++ iOS_Assessment3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Assessment3
+//
+//  Created by Ezra Kiteck on 11/8/18.
+//  Copyright © 2018 Ezra Kiteck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
